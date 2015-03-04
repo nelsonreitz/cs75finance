a5342db31403576c82dc20d46a4c0e562b1a5832
Nelson Reitz <nailsonne@gmail.com>
2015-03-04 03:28:03 +0100

    Header CSS

--- views/lookup_form.php
+++ views/lookup_form.php
@@ -1,4 +1,4 @@
 <form id="lookup-form" action="index.php" method="GET">
   <input autofocus name="symbol" placeholder="Symbol" type="text">
-  <input type="submit" value="Lookup stock">
+  <input type="submit" value="Lookup">
 </form>
