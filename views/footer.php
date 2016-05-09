35416cd9b4d8be6ec4f2bc30f3d546cf1fb812ad
Nelson Reitz <nailsonne@gmail.com>
2016-05-09 15:00:33 +0200

    Update copyright year

--- views/footer.php
+++ views/footer.php
@@ -2,7 +2,7 @@
           <p><a href="http://finance.yahoo.com">Powered by Yahoo Finance</a></p>
           <p>This is a fictive website built as an exercise for the <a href="http://cs75.tv">Computer Science E-75</a> course.</p>
           <p>More info at <a href="https://github.com/nelsonreitz/project1">github.com/nelsonreitz/project1</a>.</p>
-          <p>&copy; Nelson Reitz - 2015</p>
+          <p>&copy; Nelson Reitz - 2016</p>
         </footer>
       </div><!-- .row -->
     </div><!-- .container -->
